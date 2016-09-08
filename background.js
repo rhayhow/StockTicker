962e475f62837b3c1cd364748cb4e028bec3c431
Reid Hayhow <reid_hayhow@agilent.com>
2016-09-08 10:54:33 -0600

    Add files
    Templated from Clacks before modifications to use background.

---
+++ background.js
@@ -0,0 +1,100 @@
+var DEBUG = false;
+
+// Create data store
+var clacks = {};
+
+// return clacks headers, called by popup to get them for display
+function getClacks(tabId) {
+    return clacks[tabId];
+};
+
+// safely hide the page action.
+function hidePageAction(tabId) {
+    if (clacks[tabId]) {
+        chrome.pageAction.hide(tabId);
+    }
+}
+
+// safely show the page action.
+function showPageAction(tabId) {
+    if (clacks[tabId]) {
+        chrome.pageAction.show(tabId);
+    }
+}
+
+
+// The main listener to check each request's headers for clacks
+chrome.webRequest.onCompleted.addListener(
+    function(details) {
+        var newClacks,
+            // match case-insensitive, with or without 'X-' prefix
+            pattern = /^(X-)?(Clacks-Overhead)$/i;
+
+        // ignore background requests (where tabId === -1)
+        if (details.tabId >= 0) {
+
+            // get response headers and store those tagged as "Clacks-Overhead"
+            // or "X-Clacks-Overhead"
+            newClacks = details.responseHeaders.filter(function(header) {
+                    return pattern.test(header.name);
+                }).map(function(header) {
+                    return header.value;
+                }).join("\n");
+
+            // if there are any Clacks-Overhead headers.
+            if (newClacks) {
+                // Store the resulting string under its tab's ID.
+                // N.B. though it displays multiple messages from one request, separate
+                // requests from one page load can still overwrite each other.
+                // Note from Pete: I've change += to just = to stop it repeating itself.
+                // - related to premature deletion? - don't think so...
+                clacks[details.tabId] = newClacks;
+                showPageAction(details.tabId);
+                if (DEBUG) console.log("store");
+            }
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["responseHeaders"]
+);
+
+// clear clacks on navigation to a new page
+chrome.webNavigation.onCommitted.addListener(
+    function(details) {
+        if (details.transitionType !== "auto_subframe") {
+            delete clacks[details.tabId];
+            hidePageAction(details.tabId);
+        }
+    }
+);
+
+// listen to messages from content scripts
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        var tabId = sender.tab.id;
+
+        if (request.clacks) {
+            if (clacks[tabId]) clacks[tabId] += "\n" + request.clacks;
+            else clacks[tabId] = request.clacks;
+        }
+
+        // if there is a clacks entry for the loaded tab, show icon for that tab.
+        if (clacks[tabId]) showPageAction(tabId);
+        // if (DEBUG) console.log("shown: ", shown[tabId]);
+});
+
+// Keeps the data store clean by deleting entries for tabs when they are closed.
+chrome.tabs.onRemoved.addListener(function (tabId) {
+    if (clacks[tabId]) {
+        delete clacks[tabId];
+    }
+});
+
+// Possible code for filtering duplicate strings
+// function uniqueStrings(list) {
+//     var set = {}, i;
+//     for (i in list) {
+//         set[list[i]] = true;
+//     }
+//     return Object.keys(set);
+// }
---
+++ popup.js
@@ -0,0 +1,48 @@
+var tabId,
+    clacks;
+
+// Get the currently active tab in the focused window.
+chrome.tabs.query(
+    {active: true, lastFocusedWindow: true},
+    function(tabs) {
+        console.log("tabs: ", tabs);
+        // get the tab ID of that current tab
+        tabId = tabs[0].id;
+        // get clacks headers from background script
+        chrome.runtime.getBackgroundPage(
+            function(bg) {
+                clacks = bg.getClacks(tabId);
+                // put text in popup.html
+                document.getElementById("text").textContent = clacks;
+
+                // animate icon
+                var letterIndex = 0;
+                var interval = 500;
+                // var iconSize = 19; // icon size, 19 or 38 pixels
+
+                // put icon in popup.html
+                var iconURL = chrome.extension.getURL("images/backspindle_icons/Clacks19/Clacks-Blank-small.png");
+                console.log(iconURL);
+                document.getElementById("icon").src = iconURL;
+
+                // animator loop
+                var icon = setInterval(function() {
+                    var str;
+
+                    // get icon name
+                    if (letterIndex == clacks.length) str = "Blank";
+                    else if (clacks[letterIndex] == ' ') str = "SPACE";
+                    else str = clacks[letterIndex].toUpperCase();
+
+                    // make full string and set src in html
+                    iconURL = chrome.extension.getURL("images/backspindle_icons/Clacks19/Clacks-" + str + "-small.png");
+                    document.getElementById("icon").src = iconURL;
+
+                    // iterate
+                    letterIndex++;
+                    if (letterIndex > clacks.length) letterIndex = 0;
+                }, interval);
+            }
+        );
+    }
+);
