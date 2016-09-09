4c9a716d13f8c1d5192bafab15a975e909acca5f
Reid Hayhow <reid_hayhow@agilent.com>
2016-09-09 11:39:27 -0600

    Add better icons for large moves and use them.
    Also fix function references.

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 var pollInterval = 1000 * 60 * 5; // 5 minutes
 
-function updateStocks(callback, errorCallback, notificationCallback) {
+function updateStocks() {
 
-    var searchUrl = 'https://www.google.com/finance/info?q=NSE:A,KEYS,.INX,TNX,.DJI'
+    var searchUrl = 'https://www.google.com/finance/info?q=NSE:A,KEYS,.INX,TNX,.DJI';
     var x = new XMLHttpRequest();
     x.open('GET', searchUrl);
     // The Google image search API responds with JSON, so let Chrome parse it.
@@ -14,7 +14,7 @@ function updateStocks(callback, errorCallback, notificationCallback) {
         // Parse and process the response from Google Image Search.
         var response = x.response;
 
-        if (!response || response.length == 0) {
+        if (!response || response.length === 0) {
             errorCallback('No response from Google Finance search!');
             return;
         }
@@ -22,44 +22,45 @@ function updateStocks(callback, errorCallback, notificationCallback) {
 
         var ticker = JSON.parse(json);
         var results = "";
+        var pctChange = 0.0;
 
         for (i = 0; i < ticker.length; i++) {
-            if (ticker[i].t == 'A') {
-                var result = 'Agilent is ' + ticker[i].l_cur + "   \n";
-                result = result + '   Changed ' + ticker[i].c + "   \n";
-                var pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
-                result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
+            if (ticker[i].t === 'A') {
+                //var result = 'Agilent is ' + ticker[i].l_cur + "   \n";
+                //result = result + '   Changed ' + ticker[i].c + "   \n";
+                pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
+                //result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
 
                 showStockNotification('Agilent Update', 'A', 'Agilent', ticker[i].l_cur, ticker[i].c, pctChange);
 
             }
-            if (ticker[i].t == 'KEYS') {
+            if (ticker[i].t === 'KEYS') {
                 results = results + 'Keysight is ' + ticker[i].l_cur + "   \n";
                 //return;
             }
-            if (ticker[i].t == '.INX') {
-                var result = 'The S&P is ' + ticker[i].l_cur + "   \n";
-                result = result + '   Changed ' + ticker[i].c + "   \n";
-                var pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
-                result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
+            if (ticker[i].t === '.INX') {
+                //var result = 'The S&P is ' + ticker[i].l_cur + "   \n";
+                //result = result + '   Changed ' + ticker[i].c + "   \n";
+                pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
+                //result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
 
                 showStockNotification('S&P 500 Update', 'S&P 500', 'The S&P 500', ticker[i].l_cur, ticker[i].c, pctChange);
 
             }
-            if (ticker[i].t == 'TNX') {
-                var result = 'The 10 year is ' + ticker[i].l_cur + "   \n";
-                result = result + '   Changed ' + ticker[i].c + "   \n";
-                var pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
-                result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
+            if (ticker[i].t === 'TNX') {
+                //var result = 'The 10 year is ' + ticker[i].l_cur + "   \n";
+                //result = result + '   Changed ' + ticker[i].c + "   \n";
+                pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
+                //result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
 
                 showStockNotification('10 Year Treasury Update', 'TNX', 'The 10 Year', ticker[i].l_cur, ticker[i].c, pctChange);
 
             }
-            if (ticker[i].t == '.DJI') {
-                result = 'The Dow is ' + ticker[i].l_cur + ",";
-                result = result + '   Changed ' + ticker[i].c + "   \n";
-                var pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
-                result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
+            if (ticker[i].t === '.DJI') {
+                //result = 'The Dow is ' + ticker[i].l_cur + ",";
+                //result = result + '   Changed ' + ticker[i].c + "   \n";
+                pctChange = (ticker[i].c / ticker[i].pcls_fix) * 100;
+                //result = result + '   Changed % ' + pctChange.toFixed(2) + "   \n";
 
                 showStockNotification('Dow Jones Update', 'DJI', 'The Dow', ticker[i].l_cur, ticker[i].c, pctChange);
 
@@ -76,19 +77,29 @@ function updateStocks(callback, errorCallback, notificationCallback) {
 function showStockNotification(notificationName, titleString, stockName, current, change, pctChange) {
     var statusText = stockName + ' is ' + current + ', ' + change + ' (' + pctChange.toFixed(2) + "%)";
 
+
+
     if (Math.abs(pctChange) > .25) {
+        var upArrowString = 'images/upArrow.png';
+        if (pctChange > 1.0) {
+            upArrowString = 'images/upArrowPriority.png';
+        }
         if (pctChange >= 0) {
             chrome.notifications.create(notificationName, {
                 type: 'basic',
-                iconUrl: 'images/upArrow.png',
+                iconUrl: upArrowString,
                 title: titleString,
                 message: statusText
             }, function (notificationId) { });
         }
         else {
+            var downArrowString = 'images/downArrow.png';
+            if (pctChange > 1.0) {
+                downArrowString = 'images/downArrowPriority.png'
+            }
             chrome.notifications.create(notificationName, {
                 type: 'basic',
-                iconUrl: 'images/downArrow.png',
+                iconUrl: downArrowString,
                 title: titleString,
                 message: statusText
             }, function (notificationId) { });
@@ -128,8 +139,7 @@ function startRequest() {
     if (now < 14) {
         window.setTimeout(startRequest, pollInterval);
     }
-    else
-    {
+    else {
         chrome.notifications.create('All Done', {
             type: 'basic',
             iconUrl: 'images/icon.png',
@@ -143,4 +153,4 @@ function stopRequest() {
     window.clearTimeout(timerId);
 }
 
-onload = startRequest()
+onload = startRequest();
